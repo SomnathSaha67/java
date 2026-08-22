da215e658b30eae0f694ddf6994bda95f0c18ccd
SomnathSaha2006 <ss0326736@gmail.com>
2026-08-22 10:54:02 +0530

    Implement basic linear search returning index or -1

---
+++ day_10/LinearSearchElementFinder.java
@@ -0,0 +1,15 @@
+public class LinearSearchElementFinder{
+  public static void main(String[] args){
+    int[] arr= {1, 4, 7, 4, 6, 9, 3, 10};
+    int target= 3;
+    System.out.print(linearSearch(arr, target));
+  }
+  static int linearSearch(int[]arr, int target){
+    for (int i=0; i<arr.length; i++){
+      if (arr[i]==target){
+        return i;
+      }
+    }
+    return -1;
+  }
+}
